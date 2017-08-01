ac1c7f960206eab3386f9763236d3159739e2a6e
Moshe <pnemonic@gmail.com>
2017-08-01 13:23:04 +0300

    Uri file exposed.

--- fractals-android/app/src/main/java/com/github/fractals/SaveFileTask.kt
+++ fractals-android/app/src/main/java/com/github/fractals/SaveFileTask.kt
@@ -18,10 +18,10 @@ package com.github.fractals
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
@@ -39,26 +39,30 @@ import java.util.*
  * Task to save a bitmap to a file.
  * @author Moshe Waisberg
  */
-class SaveFileTask(protected val context: Context) : AsyncTask<Bitmap, File, File>() {
+class SaveFileTask(val context: Context) : AsyncTask<Bitmap, File, Uri>() {
+
+    private val TAG = "SaveFileTask"
 
     private val REQUEST_APP = 0x0466 // "APP"
     private val REQUEST_VIEW = 0x7133 // "VIEW"
 
     private val ID_NOTIFY = 0x5473 // "SAVE"
 
-    protected val timestampFormat: DateFormat = SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US)
+    private val IMAGE_MIME = "image/png"
 
-    protected var bitmap: Bitmap? = null
-    protected var builder: Notification.Builder? = null
+    private val timestampFormat: DateFormat = SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US)
 
-    override fun doInBackground(vararg params: Bitmap): File? {
+    private var bitmap: Bitmap? = null
+    private var builder: Notification.Builder? = null
+
+    override fun doInBackground(vararg params: Bitmap): Uri? {
         val folderPictures = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
         val folder = File(folderPictures, context.getString(R.string.app_folder_pictures))
         folder.mkdirs()
+        val file = File(folder, generateFileName())
 
         val bitmap = params[0]
         this.bitmap = bitmap
-        val file = File(folder, generateFileName())
 
         val res = context.resources
         val iconWidth = res.getDimensionPixelSize(android.R.dimen.notification_large_icon_width)
@@ -87,12 +91,27 @@ class SaveFileTask(protected val context: Context) : AsyncTask<Bitmap, File, Fil
         val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         nm.notify(ID_NOTIFY, notification)
 
+        var url: Uri? = null
         var out: OutputStream? = null
+        val mutex = Object()
         try {
             out = FileOutputStream(file)
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.close()
+            out = null
             Log.i(TAG, "save success: " + file)
-            return file
+            url = Uri.fromFile(file)
+            MediaScannerConnection.scanFile(context, arrayOf(file.absolutePath), arrayOf(IMAGE_MIME), { path: String, uri: Uri? ->
+                if ((uri != null) && !"file".equals(uri.scheme)) {
+                    url = uri
+                }
+                synchronized(mutex) {
+                    mutex.notify()
+                }
+            })
+            synchronized(mutex) {
+                mutex.wait()
+            }
         } catch (e: IOException) {
             Log.e(TAG, "save failed: " + file, e)
         } finally {
@@ -104,15 +123,15 @@ class SaveFileTask(protected val context: Context) : AsyncTask<Bitmap, File, Fil
             }
         }
 
-        return null
+        return url
     }
 
-    override fun onPostExecute(file: File?) {
+    override fun onPostExecute(file: Uri?) {
         builder!!.setOngoing(false)
 
         if ((file != null) && (bitmap != null)) {
             val intent = Intent(Intent.ACTION_VIEW)
-            intent.setDataAndType(Uri.fromFile(file), "image/png")
+            intent.setDataAndType(file, IMAGE_MIME)
             val pendingIntent = PendingIntent.getActivity(context, REQUEST_VIEW, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
             builder!!.setContentTitle(context.getText(R.string.saved_title))
@@ -133,17 +152,15 @@ class SaveFileTask(protected val context: Context) : AsyncTask<Bitmap, File, Fil
         nm.notify(ID_NOTIFY, notification)
     }
 
-    override fun onCancelled(file: File?) {
+    override fun onCancelled(file: Uri?) {
         super.onCancelled(file)
-        if (file != null) {
-            file.delete()
-        } else {
+        if (file == null) {
             val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             nm.cancel(ID_NOTIFY)
         }
     }
 
-    protected fun generateFileName(): String {
+    fun generateFileName(): String {
         return "fractal-" + timestampFormat.format(Date()) + ".png"
     }
 }
